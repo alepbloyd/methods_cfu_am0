ee1671d335130874c0eef666040868729ac778c9
alepbloyd <alexanderleeboyd@gmail.com>
2022-04-21 13:28:46 -0400

    Add .gsub, .split, .eql?, and .chr examples

--- methods_variables.rb
+++ methods_variables.rb
@@ -27,6 +27,21 @@ last_login = "12/09/2021"
 # 1: In the Ruby Docs for String methods (https://ruby-doc.org/core-3.1.0/String.html), find 4 methods you have not yet used and call them on one of the variables above. Between reading the documentation and reading the output from calling the methods, make sense of what they do.
   # Write the methods out on the lines below, with an explanation in your own words of how they work.
 
+puts user_name.gsub("coco","beepboop")
+# gsub takes two arguments. when called on a string, it checks if the first argument is contained in the string, and if it is, it replaces it with the second argument
+# above would return "beepboop_11am"
+
+puts last_login.split("/")
+# split takes one argument, a delimiter. When called on a string, it looks for the delimiter, and divides the object into new strings at the points where the delimiter is. It then returns an array of the new strings
+# above would return ["12","09","2021"]
+
+puts user_name.eql?(last_login)
+# .eql? takes one argument. When called, it checks if the argument is equal to the object it is called on. Returns true if yes, false if no.
+# above would return false
+
+puts last_login.chr
+# .chr takes no arguements. When called, it returns the first character of the object it is called on
+# above would return "1"
 
 
 # 2: Do some research (either testing out with your own code or Googling) to build an understanding of what the `!` does, when at the end of a method name. Show your understading by providing an example and writing an explanation.
