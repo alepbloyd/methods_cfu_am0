ec05075355ba62338cecc2029ff1ff61658a78c9
alepbloyd <alexanderleeboyd@gmail.com>
2022-04-21 13:35:41 -0400

    Add custom_greeting method

--- final_practice.rb
+++ final_practice.rb
@@ -18,12 +18,23 @@ end
 
 #2: Write a method named custom_greeting that prints out a greeting WITH a specific name.
 
+def custom_greeting(name)
+  puts "Hello #{name}, here is a custom greeting with you name!"
+end
+
+# puts custom_greeting("Alex")
 
 # What is the return value of your method?
+
+# the return value is nil
+
 # How many arguments did you pass your method?
+
+# I passed one argument
+
 # What data type was your argument(s)?
 
-
+# argument is a string
 
 
 #3: Write a method named square that takes in one number, and returns the square of that number
